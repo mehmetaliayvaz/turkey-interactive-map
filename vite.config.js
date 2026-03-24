b2fd34c709e6051535a7462dd8ba82cbbcd94772
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2026-03-24 21:06:28 +0300

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/turkey-interactive-map",
   plugins: [vue(), tailwindcss()],
 });
