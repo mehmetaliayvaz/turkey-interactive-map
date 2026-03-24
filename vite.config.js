1bdd532066d796f7b8979c74fa4598e2adac177a
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2026-03-24 21:14:08 +0300

    Removed vite-config base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/turkey-interactive-map",
   plugins: [vue(), tailwindcss()],
 });
